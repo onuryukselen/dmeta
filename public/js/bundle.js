9cd587446a0d26120410388a0eb9a9df1ffe9957
Onur Yukselen <onuryukselen@gmail.com>
2020-10-23 21:29:14 -0400

    db template added

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8904,7 +8904,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34667" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33123" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
