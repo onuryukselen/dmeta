407183af6003453758bab940227051a35c2e2a99
Onur Yukselen <onuryukselen@gmail.com>
2020-11-09 15:02:02 -0500

    templete updated

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ const app = express();
 app.enable('trust proxy');
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+app.locals.basedir = app.get('views'); // set basedir for pug
+
 // 1) GLOBAL MIDDLEWARES
 app.use(cors());
 app.options('*', cors());
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+/* eslint-disable node/no-unpublished-require */
+const path = require('path');
+// const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin'); //minimize js content
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: { bundle: './public/js/index.js', 'styles-libs': './public/scss/styles-libs.scss' },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, './public/dist'), //create dist directory and save it bundle.js
+    publicPath: 'dist/'
+  },
+  watch: true,
+  watchOptions: {
+    ignored: ['node_modules/**']
+  },
+  mode: 'development',
+  devtool: 'source-map', //Webpack default uses eval and throws `unsafe-eval` error
+  module: {
+    // how to import files
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader' // once css-loader calisir sonra style-loader
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'] //convert ES6 to support old browsers
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+        loader: 'file-loader'
+      },
+      {
+        test: require.resolve('jquery'),
+        loader: 'expose-loader',
+        options: {
+          exposes: ['$', 'jQuery']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanEveryAfterBuildPatterns: [path.resolve(__dirname, './public/dist'), '!*contenthash.pug']
+    }), // removes unused files in dist folder
+    new HtmlPlugin({
+      filename: 'contenthash.pug',
+      filetype: 'pug',
+      template: path.resolve(__dirname, './views/_partials/empty.pug')
+    }),
+    new TerserPlugin(), // minimize js content
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    }) // extract css into separate file
+  ]
+};
