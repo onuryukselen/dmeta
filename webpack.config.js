bfad1170910ca0e39f24a0695e4c415f660a452e
Onur Yukselen <onuryukselen@gmail.com>
2021-07-17 23:34:56 -0400

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -16,20 +16,12 @@ module.exports = {
   optimization: {
     runtimeChunk: 'single',
     splitChunks: {
-      chunks: 'all',
-      maxInitialRequests: Infinity,
-      minSize: 0,
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name(module) {
-            // get the name. E.g. node_modules/packageName/not/this/part.js
-            // or node_modules/packageName
-            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-
-            // npm package names are URL-safe, but some servers don't like @ symbols
-            return `npm.${packageName.replace('@', '')}`;
-          }
+          name: 'vendors',
+          chunks: 'all',
+          maxSize: 1000000 //or whatever size you want
         }
       }
     }
