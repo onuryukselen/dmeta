0cc99f076db8e90de3cca125a90f89133fe0b698
Onur Yukselen <onuryukselen@gmail.com>
2021-07-18 21:32:23 -0400

    updateDM script updated

--- scripts/updateDM.sh
+++ scripts/updateDM.sh
@@ -1,1 +1,6 @@
-cd .. && git stash && git pull 2>&1
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $SCRIPT_DIR/.. && git pull 2>&1
+npm install 2>&1
+pm2 restart pm2-process.json 2>&1
